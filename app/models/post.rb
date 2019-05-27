75d5b176d32ae6497a869f8ab68c498af1d2404c
github username <github email address>
2019-05-27 15:46:41 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
+  accepts_nested_attributes_for :categories
 
 
   def categories_attributes=(category_attributes)
@@ -9,5 +10,4 @@ class Post < ActiveRecord::Base
       self.post_categories.build(category: category)
     end
   end
-
 end
